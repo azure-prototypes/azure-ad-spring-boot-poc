42a6924599e348241a8e31bf241449799a142d24
Serhii Soboliev <sergiy.sobolev@gmail.com>
2021-05-05 16:02:25 +0300

    Temporary disable GRAPH API integration

--- src/main/java/com/officedepot/azureadpoc/controller/ClientController.java
+++ src/main/java/com/officedepot/azureadpoc/controller/ClientController.java
@@ -1,10 +1,18 @@
 package com.officedepot.azureadpoc.controller;
 
 
+import com.azure.identity.ClientSecretCredential;
+import com.azure.identity.ClientSecretCredentialBuilder;
+import com.microsoft.graph.authentication.IAuthenticationProvider;
+import com.microsoft.graph.authentication.TokenCredentialAuthProvider;
+import com.microsoft.graph.models.User;
+import com.microsoft.graph.requests.GraphServiceClient;
+import okhttp3.Request;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.oidc.StandardClaimAccessor;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
@@ -12,9 +20,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 
@@ -45,10 +51,34 @@ public class ClientController {
         return "index";
     }
 
-    @GetMapping("/graph")
+    @GetMapping("/me")
     @ResponseBody
-    public OAuth2AuthorizedClient graph(@RegisteredOAuth2AuthorizedClient("graph") OAuth2AuthorizedClient graphClient) {
-        return graphClient;
+    public User me(@RegisteredOAuth2AuthorizedClient("graph") OAuth2AuthorizedClient cl) {
+        OAuth2AccessToken accessToken = cl.getAccessToken();
+        final List<String> scopes = Arrays.asList("User.Read");
+
+        GraphServiceClient graphServiceClient = getClient();
+        User me = graphServiceClient
+                .me()
+                .buildRequest()
+                .select("displayName")
+                .get();
+
+        return me;
+    }
+
+    @SuppressWarnings("unchecked")
+    private GraphServiceClient<Request> getClient() {
+        final List<String> scopes = Arrays.asList("https://graph.microsoft.com/.default");
+
+        final ClientSecretCredential defaultCredential = new ClientSecretCredentialBuilder()
+                .clientId("8751c852-f58e-47d9-a16b-51df4e0f1e73")
+                .clientSecret("r89HXq8z_mZRV2m_mZlW88LI-gslWk.v88")
+                .tenantId("c06a9d9f-5fa8-4ba3-9c9f-4aae1705ad7a")
+                .build();
+
+        final IAuthenticationProvider authProvider = new TokenCredentialAuthProvider(scopes, defaultCredential);
+        return GraphServiceClient.builder().authenticationProvider(authProvider).buildClient();
     }
 
 }
